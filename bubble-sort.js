12547cdd835438e87cc4fd27aefd241452ae4148
Richa Singh <richa.singhnxt213@gmail.com>
2026-03-30 21:51:43 +0530

    sort array using bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,11 @@
+let arr = [7, 4, 1, 5, 3, 2, 6];
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+console.log(arr);
